e1bcea9ab007413121a5d7dbca5421e848920019
Arthur Almeida <arthur.1968@hotmail.com>
2021-11-10 11:05:01 -0300

    segunda atualizacao

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    #title
+
+    title = models.CharField('Título', max_length= 500, blank= False, null= False)
+    #conteudo
+    content = models.TextField('Conteudo', blank=False, null=False)
+    #data de publi
+    date_published = models.DateField('Data da Publicação', auto_now_add= True)
+    #slug
+    slug = models.SlugField('Slug', editable=False)
